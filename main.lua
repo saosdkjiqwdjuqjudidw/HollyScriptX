1db146cc730ed5272b09c9f2d506c4f661cceb03
iasjdauhduuqhauwdhqd <incnationacc@gmail.com>
2026-07-14 01:07:13 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,2 @@
 -- i mog everyone
-getgenv().SCRIPT_KEY = "KEYLESS"
-
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/1799d3ba0aa28f23c0966cde6b7112f89f10a47044ef36c5e3ddcb1f19e1362d/download"))()
+loadstring(game:HttpGet("https://vss.pandauth.com/kv/4f4a9c0645786679"))()
