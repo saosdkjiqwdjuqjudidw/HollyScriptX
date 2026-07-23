eb150ec2246923cc3adb28ad439ed8f109269499
iasjdauhduuqhauwdhqd <incnationacc@gmail.com>
2026-07-23 19:45:56 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/a75e8c01cda2fe4eb7cc2a9b4758b261.lua"))()
+=loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/c836696dccd9ab8c2e3c7c5a810c98e9.lua"))()
